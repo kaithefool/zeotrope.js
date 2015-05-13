9d60cb63c15b1ce68c4032a46667b36c7b864841
Kai Lam <kai.chun.lam@gmail.com>
2015-05-13 16:56:25 -0700

    interrupt setting for watch task

--- tasks/watch.js
+++ tasks/watch.js
@@ -3,6 +3,9 @@ module.exports = function (grunt) {
     'use strict';
 
     grunt.config.set('watch', {
+        options: {
+            interrupt: true
+        },
     	js: {
     		files: ['src/**/*', 'test/specs/**/*Spec.js'],
     		tasks: ['browserify', 'karma:test']
